fc21c41c03f18a46fcc7a1fdfa98fb62591c3651
seankalb <153946850+Skalb8865@users.noreply.github.com>
2025-04-21 21:19:17 -0400

    .

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,13 @@
-import { defineConfig } from 'vite';
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-});
+  build: {
+    rollupOptions: {
+      input: {
+        // Main Pages
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+})
