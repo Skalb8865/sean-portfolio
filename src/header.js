a43bd8c5395b37a51edf96a35e6f3164b88579e7
seankalb <153946850+Skalb8865@users.noreply.github.com>
2025-04-22 18:58:19 -0400

    .

--- src/header.js
+++ src/header.js
@@ -7,18 +7,19 @@ fetch(jsonFile)
     data.forEach((header) => {
       const { logo_first, logo_last, About, Projects, Education, Experience } = header;
       headerSelector.innerHTML += `
-        <nav class="navbar">
-        <h1 class="nav_logo">
+      <nav class="navbar">
+        <a href="/index.html" class="nav_logo">
           ${logo_first} <span>${logo_last}</span>
-        </h1>
+        </a>
         <ul class="nav_list">
           <li><a href="#" aria-label="About Section">${About}</a></li>
           <li><a href="#" aria-label="Projects Section">${Projects}</a></li>
           <li><a href="#" aria-label="Education Section">${Education}</a></li>
           <li><a href="#" aria-label="Experience Section">${Experience}</a></li>
         </ul>
-        <a class="contact_btn" href="#" aria-label="Contact Me">Contact Me</a>
-        <div class="hamburger" id="hamburger" aria-label="open menu" aria-controls="menu" aria-expanded="false"role="button">
+        <a class="github_btn" href="https://github.com/Skalb8865" aria-label="Contact Me" target="_blank">Github</a>
+        <div class="hamburger" id="hamburger" aria-label="open menu" aria-controls="menu" aria-expanded="false"
+          role="button">
           <div class="nav_bar"></div>
           <div class="nav_bar"></div>
           <div class="nav_bar"></div>
