c624fd4092ec70a456865e2341975cece1638cd2
seankalb <153946850+Skalb8865@users.noreply.github.com>
2025-04-23 01:13:25 -0400

    .

--- src/header.js
+++ src/header.js
@@ -8,12 +8,16 @@ fetch(jsonFile)
       const { Home, Projects, Education, Experience } = header;
       headerSelector.innerHTML += `
       <nav id="navbar" class="navbar">
-        <a href="/index.html#top" class="nav_logo"><i class="ri-home-4-line"></i> ${Home}</a>
+        <a class="navbar_logo" href="/index.html">
+          <img src="/sk_logo.svg" alt="Logo">
+        </a>
+        <a href="#top" class="nav_logo active"><i class="ri-home-4-line"></i> ${Home}</a>
         <ul class="nav_list">
           <li><a href="#projects_section" aria-label="Projects Section"><i class="ri-briefcase-4-line"></i> ${Projects}</a></li>
           <li><a href="#education_section" aria-label="Education Section"><i class="ri-school-line"></i> ${Education}</a></li>
           <li><a href="#expireince_section" aria-label="Experience Section"><i class="ri-building-4-line"></i> ${Experience}</a></li>
         </ul>
+        <div id="hamburger_icon" class="ri-menu-line" role="button"></div>
       </nav>
         `;
     });
@@ -28,5 +32,5 @@ fetch(jsonFile)
 //   hamburgerIcon.addEventListener("click", myFunction);
 // });
 
-/* <a class="github_btn" href="https://github.com/Skalb8865" aria-label="My Github" target="_blank">Github</a>
-<div id="hamburger_icon" class="ri-menu-line" role="button"></div> */
+
+//  <a class="github_btn" href="https://github.com/Skalb8865" aria-label="My Github" target="_blank">Github</a>
