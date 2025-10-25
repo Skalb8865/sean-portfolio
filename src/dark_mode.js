a2fb5586e853c6b023152484568c8594bdbd19c3
Sean Kalb <spkalb8865@gmail.com>
2025-10-25 18:44:45 -0400

    .

---
+++ src/dark_mode.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.body.classList.toggle("light_mode");
+
+    if (document.body.classList.contains('light_mode')) {
+        localStorage.setItem('theme', 'light_mode');
+    } else {
+        localStorage.removeItem('theme');
+    }
+}
