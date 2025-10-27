284b73d0f79f3336aaab3b7d6ad0218b9a1c6371
Sean Kalb <spkalb8865@gmail.com>
2025-10-26 22:37:57 -0400

    .

--- src/dark_mode.js
+++ src/dark_mode.js
@@ -1,9 +1,10 @@
-function myFunction() {
+addEventListener("click", (event) => {
     document.body.classList.toggle("light_mode");
+})
 
-    if (document.body.classList.contains('light_mode')) {
-        localStorage.setItem('theme', 'light_mode');
-    } else {
-        localStorage.removeItem('theme');
-    }
+if (document.body.classList.contains('light_mode')) {
+    localStorage.setItem('theme', 'light_mode');
+} else {
+    localStorage.removeItem('theme');
 }
+
